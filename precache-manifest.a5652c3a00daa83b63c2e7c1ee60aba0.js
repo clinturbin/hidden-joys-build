f3ba7783af999a47e7e980cee8e92cb87e013bf9
clinturbin <clinturbin@gmail.com>
2019-01-25 19:50:21 -0500

    Update home screen background source

---
+++ precache-manifest.a5652c3a00daa83b63c2e7c1ee60aba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6986244a63110a9ea6ec67d958fc6021",
+    "url": "/static/media/pink-smoke.6986244a.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1e82bc90044ce8c243a4",
+    "url": "/static/js/main.1e82bc90.chunk.js"
+  },
+  {
+    "revision": "08fa7acebe088493708e",
+    "url": "/static/js/1.08fa7ace.chunk.js"
+  },
+  {
+    "revision": "1e82bc90044ce8c243a4",
+    "url": "/static/css/main.01acef4d.chunk.css"
+  },
+  {
+    "revision": "4b28aa2c5128e807c6b706a99cf32f65",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8121019ee3caf5805cd6d83f06678842.js"
+  "/precache-manifest.a5652c3a00daa83b63c2e7c1ee60aba0.js"
 );
 
 workbox.clientsClaim();
